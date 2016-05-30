ea230c81da2229aae7184ba1fc4c17dcedbc5803
fscarpin <fernando.scarpin@gmail.com>
2016-05-30 13:26:23 -0300

    Added pricing methods to Stock

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    puts "test"
+
   end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,36 @@
 class Stock < ActiveRecord::Base
+
+  def self.find_by_ticker(symbol_ticker)
+    where(ticker: symbol_ticker).first
+  end
+
+  def self.new_from_lookup(symbol_ticker)
+    quotes = Stock.pricer.quotes([symbol_ticker], [:name]).first
+
+    return if !Stock.valid_quotes?(quotes)
+
+    stock = new(ticker: symbol_ticker, name: quotes.name)
+    stock.last_price = stock.price
+
+    return stock
+  end
+
+  def price
+    quotes = Stock.pricer.quotes([ticker], [:close]).first
+
+    if Stock.valid_quotes?(quotes)
+      return quotes.close
+    else
+      return "Unavailable"
+    end
+  end
+
+  private
+  def self.valid_quotes? (quotes)
+    return !(quotes.name == "N/A")
+  end
+
+  def self.pricer
+    return YahooFinance::Client.new
+  end
 end
