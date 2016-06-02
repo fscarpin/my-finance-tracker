3c1816ef2b4ad9906a971a176611fcb84f02f4c0
fscarpin <fernando.scarpin@gmail.com>
2016-06-02 18:06:22 -0300

    Added a spin when the stocks are being searched. Also fixed the grid alignment

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -6,9 +6,18 @@ init_stock_lookup = function() {
     init_stock_lookup();
   });
 
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status){
+    show_spinner();
+  });
+
+  $('#stock-lookup-form').on('ajax:after', function(event, data, status){
+    hide_spinner();
+  });
+
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
     $('#stock-lookup-results').replaceWith('');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
+    hide_spinner();
   });
 }
 
